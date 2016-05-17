444663b23f975229747d7f87831d4a20a664813b
chukwurahb <chukwurahb@outlook.com>
2016-05-18 00:38:53 +0100

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,1 +1,19 @@
 __author__ = 'sus'
+import requests
+from bs4 import BeautifulSoup
+
+def naira_land_spider(max_pages):
+    page=1
+    while page <= max_pages:
+        url='http://www.nairaland.com/'+str(page)
+        source_code= requests.get(url)
+        plain_text=source_code.text
+        soup= BeautifulSoup(plain_text)
+        for link in soup.findAll('a',{'rel':'noopener'}):
+            href = link.get('href')
+            print(href)
+        page +=1
+
+naira_land_spider(1)
+
+
